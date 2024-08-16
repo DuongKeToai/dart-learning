bde2142b9df5c0293d35b98303de37dc18c09f66
DuongKeToai <duongketoai2342000@gmail.com>
2024-08-16 22:27:46 +0700

    buoi1

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,5 +1,42 @@
 import 'package:dart_learning/dart_learning.dart' as dart_learning;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_learning.calculate()}!');
+ // print('Hello world: ${dart_learning.calculate()}!');
+
+ //var
+
+//   var age = 25;
+//   print("[Var] Giá Trị của Age là $age");
+//   age = 20;
+//   print("[Var] Giá trị của Age là $age");
+
+//   //final
+//   //final year = 2024;
+//   //print("This year: $year");
+
+//   //const
+//  // const moth = 0;
+
+//   //dynamic
+//   dynamic a = "Toại";
+//   print("[Dynamic] Gia trị của a là $a");
+//   a = 1;
+//   print("[Dynamic] Giá trị của a sau khi thay đổi kiểu dữ liệu là : $a");
+
+//   //int , double,  String , bool, List
+//   // Biến để lữu trữ và quản lý dữ liệu 
+//   // Bạn có thể khai báo với hoặc ko cần chỉ định dữ liệu 
+//   // Sử dụng final và const để khai báo biến mà bạn có thể muốn thay đổi (gán 1 lần duy nhất)
+
+//   //so sánh final và const
+//   //const biến được khai báo trước ở thời điểm compile
+//   const b = 2000;
+//   print("giá trị của b: $b");
+
+//   final String c;
+//   print("wait 1");
+//   print("wait 2");
+//   print("wait 3");
+//   c = "toai";
+//   print("Giá trị của c: $c");
 }
